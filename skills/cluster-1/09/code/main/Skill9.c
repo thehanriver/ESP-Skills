e2c3732d8f3c5d746e4f67082ab3dad3e323fb5c
Mario Han <45515930+thehanriver@users.noreply.github.com>
2020-09-21 12:00:17 -0400

    Update Skill9.c

--- skills/cluster-1/09/code/main/Skill9.c
+++ skills/cluster-1/09/code/main/Skill9.c
@@ -3,6 +3,8 @@
    Unless required by applicable law or agreed to in writing, this
    software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
    CONDITIONS OF ANY KIND, either express or implied.
+   
+   Mario Han 9/21/20
 */
 #include <stdio.h>
 
