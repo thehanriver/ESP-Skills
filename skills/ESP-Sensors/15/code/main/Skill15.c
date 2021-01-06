68012125a1b3b4baf276cc8be11fda8edd32dfc7
marioisgay123 <hanmario@bu.edu>
2021-01-05 16:12:33 -0800

    update

---
+++ skills/ESP-Sensors/15/code/main/Skill15.c
@@ -0,0 +1,109 @@
+/* ADC1 Example
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+
+   Mario Han 9/30/20
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+#include "driver/uart.h"
+#include "driver/adc.h"
+#include "esp_adc_cal.h"
+#include "sdkconfig.h"
+#include "esp_vfs_dev.h"
+#include <math.h>
+
+#define DEFAULT_VREF    1100        //Use adc2_vref_to_gpio() to obtain a better estimate
+#define NO_OF_SAMPLES   10          //Multisampling
+
+static esp_adc_cal_characteristics_t *adc_chars;
+static const adc_channel_t channel = ADC_CHANNEL_6;     //GPIO34 if ADC1, GPIO14 if ADC2
+static const adc_atten_t atten = ADC_ATTEN_DB_11;
+static const adc_unit_t unit = ADC_UNIT_1;
+////////////////////////////////////////////////////////////////////////////////
+
+static void check_efuse(void)
+{
+    //Check TP is burned into eFuse
+    if (esp_adc_cal_check_efuse(ESP_ADC_CAL_VAL_EFUSE_TP) == ESP_OK) {
+        printf("eFuse Two Point: Supported\n");
+    } else {
+        printf("eFuse Two Point: NOT supported\n");
+    }
+
+    //Check Vref is burned into eFuse
+    if (esp_adc_cal_check_efuse(ESP_ADC_CAL_VAL_EFUSE_VREF) == ESP_OK) {
+        printf("eFuse Vref: Supported\n");
+    } else {
+        printf("eFuse Vref: NOT supported\n");
+    }
+}
+
+static void print_char_val_type(esp_adc_cal_value_t val_type)
+{
+    if (val_type == ESP_ADC_CAL_VAL_EFUSE_TP) {
+        printf("Characterized using Two Point Value\n");
+    } else if (val_type == ESP_ADC_CAL_VAL_EFUSE_VREF) {
+        printf("Characterized using eFuse Vref\n");
+    } else {
+        printf("Characterized using Default Vref\n");
+    }
+}
+
+static void voltage_reading() {
+  //Check if Two Point or Vref are burned into eFuse
+  check_efuse();
+  //Configure ADC
+  if (unit == ADC_UNIT_1) {
+      adc1_config_width(ADC_WIDTH_BIT_12);
+      adc1_config_channel_atten(channel, atten);
+  } else {
+      adc2_config_channel_atten((adc2_channel_t)channel, atten);
+  }
+  //Characterize ADC
+  adc_chars = calloc(1, sizeof(esp_adc_cal_characteristics_t));
+  esp_adc_cal_value_t val_type = esp_adc_cal_characterize(unit, atten, ADC_WIDTH_BIT_12, DEFAULT_VREF, adc_chars);
+  print_char_val_type(val_type);
+  //Continuously sample ADC1
+  while (1) {
+      uint32_t adc_reading = 0;
+      //Multisampling
+      for (int i = 0; i < NO_OF_SAMPLES; i++) {
+          if (unit == ADC_UNIT_1) {
+              adc_reading += adc1_get_raw((adc1_channel_t)channel);
+              vTaskDelay(100 / portTICK_PERIOD_MS);
+          }
+      }
+      adc_reading /= NO_OF_SAMPLES;
+      //Convert adc_reading to voltage in mV
+      float distance;
+      double voltage = esp_adc_cal_raw_to_voltage(adc_reading, adc_chars);
+      voltage /= 1000;
+      if (voltage > 2)
+        {
+            distance = (30 / (voltage - 1));
+        }
+        else
+        {
+            distance = (60 / (voltage - 0.1));
+        }
+      //distance *= 1000;
+      printf("Raw: %d\t volt: %f\t distance in cm: %f\n", adc_reading, voltage, distance);
+      vTaskDelay(pdMS_TO_TICKS(1000));
+  }
+}
+
+void app_main(void)
+{
+    ESP_ERROR_CHECK(uart_driver_install(UART_NUM_0, 256,0,0,NULL,0));
+    esp_vfs_dev_uart_use_driver(UART_NUM_0);
+    xTaskCreate(voltage_reading, "voltage_reading",4096, NULL, 4, NULL);
+
+}
--- skills/cluster-2/16/code/readings.js
+++
@@ -1,9 +0,0 @@
-const Readline = require ("@serialport/parser-readline");
-const SerialPort = require ("serialport");
-const port = new SerialPort("COM4", {baudRate: 115200});
-
-const parser = new Readline();
-port.pipe(parser);
-
-parser.on("data", (line) => console.log(`> ${line}`));
-port.write("Power On\n");
